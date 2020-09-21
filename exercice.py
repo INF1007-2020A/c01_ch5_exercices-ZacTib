c30990b72445643e6c88a0c1868f75e9237fe169
ZacTib <60437079+ZacThib@users.noreply.github.com>
2020-09-21 14:00:44 -0400

    exos termines

--- exercice.py
+++ exercice.py
@@ -3,28 +3,61 @@
 
 
 from typing import List
+import math
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    print("Veuillez entrer un nombre : ")
+    nombre = int(input())
+    return math.fabs(nombre)
+
 
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixes = 'JKLMNOP', 'ack'
+    prefixes, suffixes = 'JKLMNOPQ', 'ack'
+    chaine = []
+    for i in prefixes :
+        chaine.append(i + suffixes)
 
-    return [""]
+    return chaine
 
 
 def prime_integer_summation() -> int:
-    return 0
+    primes = []
+    x = 2
+    cpt = 0
+    while(cpt < 100):
+        for i in range(1,math.ceil(math.sqrt(x))):
+            if (x % i == 0 and i != 1):
+                break
+            if (i == math.floor(math.sqrt(x))):
+                primes.append(x)
+                cpt += 1
+        x += 1
+    return sum(primes)
+
+
+
+
+
+
+
+
+
+
 
 
 def factorial(number: int) -> int:
-    return 0
+    return math.factorial(number)
+
+
 
 
 def use_continue() -> None:
-    pass
+    for i in range(10) :
+        if(i == 5) :
+            continue
+        print(i)
 
 
 def main() -> None:
